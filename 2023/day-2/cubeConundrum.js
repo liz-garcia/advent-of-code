fae6019ef2efc4f37fbe91439e72c5a55b3ec415
liz-garcia <lizgarciamarin@gmail.com>
2024-04-29 16:04:26 +0200

    Added comment.

--- 2023/day-2/cubeConundrum.js
+++ 2023/day-2/cubeConundrum.js
@@ -3,4 +3,18 @@ import fs from "fs";
 const puzzleInputPath = "2023/day-2/puzzleInput.txt";
 const puzzleInput = fs.readFileSync(puzzleInputPath, "utf-8").split("\n");
 
-console.log(puzzleInput);
+/* Which games would have been possible if the bag contained (in total) only `12 red, 13 green, 14 blue`. That is:
+
+    --> Evaluate each GAME to determine if its SUBSETS accomplish the following conditions:
+    - per SUBSET, Red values are always 12 or less.
+    - per SUBSET, Green values are always 13 or less.
+    - per SUBSET, Blue values are always 14 or less.
+    - Total cubes per SUBSET are always 39 or less (in case there's randomly an extra color in any subset?)
+
+    --> Consider:
+    - Each GAME includes its ID in its name, like: Game 11.
+    - SUBSETS are 1 or more per GAME.
+    - SUBSETS are separated by semi-colons ;
+    - SUBSETS may return all 3 colors  or less colors. (or none?). Not always every color exists within the SUBSET.
+    - Final Answer will be equivalent to the SUM of the IDs of all POSSIBLE games.
+*/
